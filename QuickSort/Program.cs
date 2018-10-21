4658c494d66c290e0e369637ac486e51305243f6
LevonyanEdgar <edgarlevonyandp@gmail.com>
2018-10-21 20:34:50 +0400

    v0.6

---
+++ QuickSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, -1, -3, 62, 12, 1, 2, 66, -146, 721, -29 };
+            int[] arr2 = { 1, -56, 7, 1, 14, 8, 7, 1, 6, 1, 5 };
+
+           QuickSortClass.Sort(ref arr, 0, arr.Length);
+            Console.WriteLine("all good");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            
+        }
+
+
+    }
+   
+}
+
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+   static class QuickSortClass
+    {
+        public static void Sort(ref int[] arr, int start, int finish)
+        {
+            if (start<finish)
+            {
+                int position = Pivot(ref arr, start, finish);
+                if (start <position)
+                {
+                    Sort(ref arr, start, finish);
+                }
+                if (finish>position)
+                {
+                    Sort(ref arr, position+1, finish);
+
+                }
+
+            }
+
+
+        }
+
+        private static int Pivot(ref int[] arr, int start, int finish)
+        {
+            if (start-finish==0)
+            {
+                return start;
+            }
+            int a = start + 1;
+            int b = start + 1;
+            for (int i =start+1; i < finish; i++)
+            {
+                if (arr[start]<=arr[i])
+                {
+
+
+                    a++;
+                }
+                else
+                {
+                    Swap(ref arr[a], ref arr[b]);
+                    a++;
+                    b++;
+
+
+                }
+            }
+            Swap(ref arr[start], ref arr[b-1]);
+            return b - 1;
+
+        }
+
+        private static void Swap(ref int v1, ref int v2)
+        {
+            int temp = v1;
+            v1 = v2;
+            v2=temp;
+        }
+    }
+}
